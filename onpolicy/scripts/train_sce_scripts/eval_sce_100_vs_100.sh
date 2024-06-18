0f2a3311f2c9dc9648a086ebba932185fc411f81
sunfeng2016 <sunfeng@nuaa.edu.cn>
2024-06-18 17:22:11 +0800

    fix shell

--- onpolicy/scripts/train_sce_scripts/eval_sce_100_vs_100.sh
+++ onpolicy/scripts/train_sce_scripts/eval_sce_100_vs_100.sh
@@ -2,13 +2,11 @@
 env="SCE"
 map="100_vs_100"
 algo="rmappo"
-exp="train"
+exp="eval"
 seed_max=1
 
-run_cmd="python"
-# run_cmd="python -m debugpy --listen 8888 --wait-for-client"
-
-source /home/ubuntu/.conda/envs/sunfeng/bin/activate pymarl
+# run_cmd="python"
+run_cmd="python -m debugpy --listen 8888 --wait-for-client"
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
@@ -16,5 +14,6 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 ${run_cmd} ../train/train_sce.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --map_name ${map} --seed ${seed} --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 \
-    --num_env_steps 10000000 --ppo_epoch 10 --use_value_active_masks --use_eval --eval_episodes 32
+    --num_env_steps 100000 --ppo_epoch 10 --use_value_active_masks --use_eval --eval_episodes 32 \
+    --only_eval True --model_dir '/home/ubuntu/sunfeng/MARL/on-policy/onpolicy/scripts/results/SCE/defense/100_vs_100/rmappo/check/wandb/run-20240615_231926-cbd5yhu4/files'
 done
